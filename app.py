5c232b54e94158b1aa322fab24a11e169ed420f5
PrakruthiMadhav <prakruthimadhav@gmail.com>
2025-02-06 11:59:02 +0530

    Initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/verify/operation', methods=['GET'])
-def health_check():
+def verify():
     return jsonify({"health": "All systems operational"})
 
 if __name__ == '__main__':
